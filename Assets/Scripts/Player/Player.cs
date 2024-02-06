7c95161933fb138307e0bcc5be0a7eb99116f316
LAPURACHULETA <101416312+LAPURACHULETA@users.noreply.github.com>
2024-02-05 18:05:22 -0600

    Correr/cambio de vista
    se termino y se pulió la logia de correr
    se implemento el cambio de vista de 3era a 1era persona
    se organizo el código para tener una mejor visibilidad en el inspector

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -7,58 +7,89 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    [Header("Movimiento Player")]
+    [Space(10)]
     [SerializeField] private float speed;
+    [SerializeField] private float timeCanRunPlayer, recoveryTime;
+    [SerializeField] private bool canRunPlayer;
+    private float maxSpeed;
 
+    [Header("Cambio de Vista")]
+    [Space(10)]
+    [SerializeField] private Transform firstPerson;
+    [SerializeField] private Transform thirdPerson;
+    [SerializeField] private Transform cameraPerson;
+    [SerializeField] private bool canChangePerson;
+    
     private Rigidbody rb;
     private PlayerInput playerInput;
     private Vector2 input;
-    [SerializeField] private bool canRunPlayer;
-    private float timeRun;
-    private float timeRestRun;
+   
     // Start is called before the first frame update
     void Start()
     {
+
+        cameraPerson.position = Vector3.Lerp(cameraPerson.position, thirdPerson.position, 5 * Time.deltaTime);
+        cameraPerson.SetParent(thirdPerson);
         rb = GetComponent<Rigidbody>();
         playerInput = GetComponent<PlayerInput>();
+        maxSpeed = speed;
         canRunPlayer = false;
     }
     private void FixedUpdate()
     {
         /// <summary>
-        /// mmovimient del personaje
+        /// movimiento del personaje
         /// </summary>
         input = playerInput.actions["Move"].ReadValue<Vector2>();
         rb.AddForce(new Vector3(input.x, 0f, input.y) * speed);
-        Debug.Log(speed);
         /// <summary>
-        /// contro de correr del personaje
+        /// control de recuperaion del personaje
         /// </summary>
         if (canRunPlayer)
         {
-            timeRun += Time.deltaTime;
+            timeCanRunPlayer += Time.deltaTime;
+            if (timeCanRunPlayer > recoveryTime)
+            {
+                canRunPlayer = false;
+                timeCanRunPlayer = 0;
+            }
         }
-        if (timeRun >= 2)
-        {
-            canRunPlayer = false;
-            speed -= 5f; 
-            timeRun = 0;
-        }
-        if(!canRunPlayer)
-        {
-            timeRestRun += Time.deltaTime;
-        }
-        if(timeRestRun >= 2)
-        {
-            canRunPlayer=true;
-            timeRestRun = 0;
-        }
+        
     }
     public void RunPlayer(InputAction.CallbackContext context)
     {
         if(context.performed && !canRunPlayer)
         {
-            canRunPlayer = true;
             speed += 5;
+            Invoke("StopToRun", 2f);
+            canRunPlayer = true;
         }
+    }
+    public void ChangePersone(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            canChangePerson = true;
+        } 
+        if (context.canceled)
+        {
+            canChangePerson = false;
+        }
+        if (canChangePerson)
+        {
+            cameraPerson.position = Vector3.Lerp(transform.position, firstPerson.position, 1000 * Time.deltaTime);
+            cameraPerson.SetParent(firstPerson);
+        }
+        if (!canChangePerson)
+        {
+            cameraPerson.position = Vector3.Lerp(transform.position, thirdPerson.position, 1000 * Time.deltaTime);
+            cameraPerson.SetParent(thirdPerson);
+        }
+    }
+  
+    private void StopToRun()
+    {
+        speed = maxSpeed;
     }
 }
